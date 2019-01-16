0999e43b982fb398be9e1f504f74086cc221a052
Joe Ward <joethermal@gmail.com>
2019-01-16 16:30:15 +1300

    Initial Commit

---
+++ Login Do Try Catch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Login Do Try Catch
+//
+//  Created by Joseph Ward on 16/01/19.
+//  Copyright © 2019 Joethermal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
